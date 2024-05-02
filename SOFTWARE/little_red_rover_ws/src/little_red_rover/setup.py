261b1cb185009f0d9ca53f1773968beeebdb4ec0
usedhondacivic <micacer48@gmail.com>
2024-05-02 10:50:55 -0400

    Add rover urdf

--- SOFTWARE/little_red_rover_ws/src/little_red_rover/setup.py
+++ SOFTWARE/little_red_rover_ws/src/little_red_rover/setup.py
@@ -12,12 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('assets/rover/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
-    maintainer_email='micacer48@gmail.com',
+    maintainer_email='michael@michael-crum.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
