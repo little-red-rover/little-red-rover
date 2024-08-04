62b1af8885e5effb737ff0d0139f44763775ad4e
usedhondacivic <micacer48@gmail.com>
2024-08-03 19:25:58 -0700

    app-colcon.meta working

--- SOFTWARE/esp32_firmware/main/lrr_micro_ros.c
+++ SOFTWARE/esp32_firmware/main/lrr_micro_ros.c
@@ -45,30 +45,11 @@ void app_main(void)
 
     LSM6DS3_imu_driver_init();
 
-    // drive_base_driver_init();
+    drive_base_driver_init();
 
     lidar_driver_init();
 
     wifi_mgr_init();
 
-    // This has got to be allocating memory incorrectly
-    nav_msgs__msg__Odometry *odom_msg = nav_msgs__msg__Odometry__create();
-    std_msgs__msg__Int32 int32_msg;
-    rcl_publisher_t *odom_publisher = register_publisher(
-      ROSIDL_GET_MSG_TYPE_SUPPORT(nav_msgs, msg, Odometry), "odom_ig");
-    rcl_publisher_t *int2_publisher = register_publisher(
-      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "int32_testing2");
     micro_ros_mgr_init();
-
-    while (get_uros_state() != AGENT_CONNECTED) {
-    }
-
-    int32_msg.data = 0;
-    while (1) {
-        ESP_LOGI(":(", ":( before");
-        int32_msg.data++;
-        RCCHECK(rcl_publish(int2_publisher, &int32_msg, NULL));
-        // RCCHECK(rcl_publish(odom_publisher, odom_msg, NULL));
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
 }
